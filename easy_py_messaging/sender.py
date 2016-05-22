4e9382b507cec61b964dbc2d3b3be08e75fc18c0
Trailing Dots <trailingdots@gmail.com>
2016-05-22 13:25:22 -0700

    Started a section on various messaging examples. Mulitple new images.

---
+++ easy_py_messaging/sender.py
@@ -0,0 +1,146 @@
+#!/bin/env python
+#
+# Example of sending messages and handling responses.
+#
+
+
+import sys
+import os
+import time
+import zmq
+import json
+import pickle
+import atexit
+
+import platform
+from send_recv_task import SendRecvTask
+
+NOISY = True
+
+# The port used for communicatingcommunicating.
+PORT = 5590
+
+'''
+def usage():
+    """Print the usage blurb and exit."""
+    print 'dirSvc [--help] [--port] [--memory-file=memory_filename]'
+    print '\t\t[--clear]'
+    print '\t--help         = This blurb'
+    print '\t--port=aport   = Port to expect queries.'
+    print '\t--memory-file=memory_filename   = File to persist names'
+    print '\t\tDefault: ./dirSvc.data'
+    print '\t--clear        = Clear memory-file upon starting.'
+    print '\t\tDefault: False, do not clear but load memory-file'
+    print ''
+    sys.exit(1)
+
+
+def parseOpts():
+    import getopt
+    global NOISY
+    global PORT
+
+    try:
+        opts, args = getopt.gnu_getopt(
+            sys.argv[1:], 'cpmh',
+            ['port=',           # Port # to expect messages
+             'memory-file=',    # Which file to persist names
+             'help',            # Help blurb
+             'noisy',           # Turn noise on
+             'clear'            # If set, clean memory-file at start
+            ]
+        )
+    except getopt.GetoptError as err:
+        print err
+        usage()
+
+    # Number leading args to shift out
+    shift_out = 0
+    config = {
+            'clear': False,
+            'memory_filename': './dirSvc.data',
+            'port': str(PORT),
+            }
+    for opt, arg in opts:
+        if opt in ['-h', '--help']:
+            usage()
+        elif opt in ['--noisy']:
+            NOISY = True
+            continue
+        elif opt in ['p', '--port']:
+            try:
+                # Ensure a valid integer port
+                int_port = int(arg)
+            except Exception as err:
+                sys.stdout.write(str(err) + '\n')
+                usage()
+            config['port'] = arg
+            PORT = int_port
+            shift_out += 1
+            continue
+        elif opt in ['m', '--memory-file']:
+            shift_out += 1
+            config['memory_filename'] = arg
+            continue
+        elif opt in ['c', '--clear']:
+            shift_out += 1
+            config['clear'] = True
+            continue
+    # pass the remaining args to the rest of the program.
+    for ndx in range(shift_out):
+        del sys.argv[1]
+
+    return config
+'''
+
+def main():
+    """
+    Main processing loop.
+    The ZeroMQ pattern is The Lazy Pirate
+    """
+    global PORT
+
+    #config = parseOpts()
+    context = zmq.Context(1)
+    server = context.socket(zmq.REP)
+    port = PORT
+    try:
+        # Listen to all traffic both local and remote on this port.
+        server.bind("tcp://*:%s" % str(port))
+        sys.stdout.write('bound to port %d\n' % port)
+    except zmq.ZMQError as err:
+        sys.stderr.write('ZMQError: %s\n' % err)
+        sys.stderr.write('Please kill other instances of this program.\n')
+        sys.stderr.write('Or: another program may be using port %s\n' %
+            str(port))
+        sys.exit(1)
+
+    sys.stdout.write('sender started. pid %s port %s\n' %
+        (str(os.getpid()), str(port)))
+
+    config = {
+        'port': 5590,
+        'id_name': platform.node(),
+    }
+    send_recv = SendRecvTask(config)
+
+    sequence = 0
+    while True:
+        # Wait for a port naming request.
+        # Notice this recv waits forever. This implies
+        # a dirty directory will not get cleared.
+        # Should a timeout change this logic?
+        if NOISY: print("I: Normal send/recv port: %s)" % port)
+        request = 'Msg %d' % sequence
+        response = send_recv.send(request)
+        if NOISY: print("I: Normal response (%s:%s)" % (request, response))
+        if str(response) == '@EXIT':
+            break
+        sequence += 1
+
+    # Shut down ZeroMQ sockets in an orderly manner.
+    server.close()
+    context.term()
+
+if __name__ == '__main__':
+    main()
